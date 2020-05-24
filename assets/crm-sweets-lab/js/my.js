9bd8439f29736aa94860529bbcb7ead51a96faf6
Strify25 <mozhartsevr@gmail.com>
2020-05-24 16:28:53 +0300

    сделал корзину

--- assets/crm-sweets-lab/js/my.js
+++ assets/crm-sweets-lab/js/my.js
@@ -13,4 +13,40 @@ jQuery(document).ready(function($) {
 			$(this).val("8");
 		}
 	});
+
+	function prodPlus(el) {
+    	el.click(function () {
+			var $input = $(this).parent().find('input');
+			if ($input.val() == $input.attr("max")) {
+				var res = '<div class="uk-flex-top" data-uk-modal><div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical"><button class="uk-modal-close-default" type="button" data-uk-close></button><p class="uk-text-center">Вы выбрали максимальное количество товара</p></div></div>';
+				UIkit.modal(res).toggle();
+				return false;
+			}
+			$input.val(parseInt($input.val()) + 1);
+			$input.change();
+			return false;
+		});
+    }
+
+	function prodMinus(el) {
+		el.click(function () {
+			var $input = $(this).parent().find('input');
+			var count = parseInt($input.val()) - 1;
+			count = count < 1 ? 1 : count;
+			$input.val(count);
+			$input.change();
+			return false;
+		});
+	}
+
+	prodPlus($('.prodQuant .plus'));
+    prodMinus($('.prodQuant .minus'));
+
+    $(document).on('change', '.quatityChange', function(event) {
+    	event.preventDefault();
+    	var price = $(this).parents("tr").find('.price').text();
+    	var summ = price*$(this).val();
+
+    	$(this).parents("tr").find('.summ').text(summ);
+    });
 });
